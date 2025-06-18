c2643caa619a12ca73c5a99caade6f270f000306
Catherine-and <149893524+Catherine-and@users.noreply.github.com>
2025-06-18 17:42:25 +0300

    Initial Commit

---
+++ OnlineCyprusBuses/OnlineCyprusBusesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineCyprusBusesApp.swift
+//  OnlineCyprusBuses
+//
+//  Created by Ekaterina Isaeva on 18/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct OnlineCyprusBusesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnlineCyprusBusesTests/OnlineCyprusBusesTests.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineCyprusBusesTests.swift
+//  OnlineCyprusBusesTests
+//
+//  Created by Ekaterina Isaeva on 18/06/2025.
+//
+
+import Testing
+@testable import OnlineCyprusBuses
+
+struct OnlineCyprusBusesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
