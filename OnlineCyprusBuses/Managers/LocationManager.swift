ef7e72fa3ca0fdfb415fea2c90c76a63169632cb
Catherine-and <149893524+Catherine-and@users.noreply.github.com>
2025-06-23 16:52:25 +0300

    User location integration

---
+++ OnlineCyprusBuses/Managers/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  OnlineCyprusBuses
+//
+//  Created by Ekaterina Isaeva on 20/06/2025.
+//
+
+import Foundation
---
+++ OnlineCyprusBuses/Utilities/MapUtilities.swift
@@ -0,0 +1,8 @@
+//
+//  MapUtilities.swift
+//  OnlineCyprusBuses
+//
+//  Created by Ekaterina Isaeva on 20/06/2025.
+//
+
+import Foundation
